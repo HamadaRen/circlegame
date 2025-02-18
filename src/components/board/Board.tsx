74d22fae2dd118a50b4606d91ad4f2a851634026
HamadaRen <hamada@two-six.jp>
2025-02-18 11:39:28 +0900

    勝利までの流れ作成

---
+++ src/components/board/Board.tsx
@@ -0,0 +1,90 @@
+import { Cell } from './cell/Cell';
+import './Board.css';
+import { useEffect, useState } from 'react';
+import React from 'react';
+
+export const Board = () => {
+  const [count, setCountUp] = useState<number>(0);
+  const [isMark, setIsMark] = useState<'circle' | 'cross' | null>(null);
+  const [circleArray, setCircleArray] = useState<number[]>([]);
+  const [crossArray, setCrossArray] = useState<number[]>([]);
+
+  const [winMsg, setWinMsg] = useState<string>('');
+
+  let array: number[] = [];
+  for (let num = 1; num <= 9; num++) {
+    array.push(num);
+  }
+
+
+  const lines: number[][] = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+  ];
+
+  useEffect(() => {
+    if(circleArray.length <= 2){
+      return;
+    }else{
+      const resultCircle = lines.map((numArray) => numArray.every((value) => circleArray.includes(value)))
+      
+      console.log(resultCircle)
+      if (resultCircle.includes(true)) {
+        setWinMsg('○の勝ち');
+        return;
+      }
+    }
+    
+    // const resultCircle = lines[0].every((num) => circleArray.includes(num));
+    
+
+    if(crossArray.length <= 2){
+      return;
+    }else{
+      const resultCross = lines.map((numArray) => numArray.every((value) => crossArray.includes(value)))
+  
+      console.log(resultCross)
+      if (resultCross.includes(true)) {
+        setWinMsg('×の勝ち');
+        return;
+      }
+    }
+    // const resultCross = lines[0].every((num) => crossArray.includes(num))
+
+  }, [circleArray, crossArray]);
+
+  // if (circleArray.includes(lines[0])) {
+  //   alert('○の勝ち')
+  // } else if (crossArray.includes(lines[0])) {
+  //   alert('×の勝ち');
+  // }
+
+  return (
+    <>
+      <div className="board">
+        {array.map((num) => (
+          <Cell
+            key={num}
+            id={num}
+            count={count}
+            setCountUp={setCountUp}
+            isMark={isMark}
+            setIsMark={setIsMark}
+            circleArray={circleArray}
+            crossArray={crossArray}
+            setCircleArray={setCircleArray}
+            setCrossArray={setCrossArray}
+          />
+        ))}
+      </div>
+      {winMsg && <h1>{winMsg}</h1>}
+    </>
+  );
+};
+export default Board;
---
+++ src/components/board/cell/Cell.tsx
@@ -0,0 +1,70 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import './Cell.css';
+import { count } from 'console';
+import circle from '../../../circle.png';
+import cross from '../../../cross.png';
+
+type cellProps = {
+  id: number;
+  count: number;
+  setCountUp: React.Dispatch<React.SetStateAction<number>>;
+  isMark: 'circle' | 'cross' | null;
+  setIsMark: React.Dispatch<React.SetStateAction<'circle' | 'cross' | null>>;
+  circleArray: number[]
+  crossArray:number[]
+  setCircleArray:React.Dispatch<React.SetStateAction<number[]>>
+  setCrossArray:React.Dispatch<React.SetStateAction<number[]>>
+};
+export const Cell = ({ id, count, setCountUp, isMark, setIsMark, circleArray, crossArray, setCircleArray, setCrossArray }: cellProps) => {
+  [isMark, setIsMark] = useState<'circle' | 'cross' | null>(null);
+
+  const countUp = () => {
+    setCountUp(count + 1);
+  };
+
+  // let circleArray: number[] = []
+  // let crossArray: number[] = []
+  const handleChange = () => {
+    if (isMark === null) {
+      {
+        count % 2 === 0 ? setIsMark('circle') : setIsMark('cross');
+      }
+      if (count % 2 === 0) {
+        setCircleArray([...circleArray, id]);
+      } else if (count % 2 !== 0) {
+        setCrossArray([...crossArray, id]);
+      }
+      countUp();
+    } else if (isMark === 'circle' || isMark === 'cross') {
+      alert('そこには置くことはできません');
+    }
+  };
+  
+  useEffect(() => {
+    setIsMark(null);
+  }, [id]);
+
+  console.log(id);
+  return (
+    <button className="cell" disabled onClick={handleChange} style={{ overflow: 'hidden', background: '' }}>
+      <div style={{ height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        {/* {isMark === 'circle' ? (
+          <img src={circle} alt="" style={{ width: '100%' }} />
+        ) : (
+          isMark === 'cross' && <img src={cross} alt="" style={{ width: '100%' }} />
+        )} */}
+
+        <img
+          src={isMark === 'circle' ? circle : isMark === 'cross' ? cross : ''}
+          alt=""
+          style={{ width: '100%', height: '100%' }}
+        />
+
+        {/* {id} */}
+      </div>
+    </button>
+  );
+};
+
+export default Cell;
